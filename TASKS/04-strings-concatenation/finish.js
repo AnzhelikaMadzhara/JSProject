1d557e21d37971c3ee92695e9ba9f58cbca1c4ee
AnzhelikaMadzhara <angelika.m1302@gmail.com>
2024-01-14 12:31:26 +0200

    Task 3 and 4

--- TASKS/04-strings-concatenation/finish.js
+++ TASKS/04-strings-concatenation/finish.js
@@ -10,3 +10,16 @@
  *
  * 3. Выведите значение последней переменной в консоль
  */
+
+const name = 'Anzhelika'
+
+const surname = 'Madzhara'
+
+const prof = 'Acounter'
+
+const presentation = 'My name is ' + name + ' ' + surname + ' and my profesion is ' + prof
+const presentation2 = `My name is ${name} ${surname} and my profesion is ${prof}`
+
+console.log(presentation)
+console.log(presentation2)
+
